e7069c6833003edd6dd53a536463b5d7cadbe700
peterek333 <piotrekpod333@gmail.com>
2020-01-11 20:29:45 +0100

    Update rabbitmq_docker.sh

--- rabbitmq_docker.sh
+++ rabbitmq_docker.sh
@@ -1,1 +1,1 @@
-docker run -d --hostname editnow-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+docker run -d --hostname editnow-rabbitmq -p 15672:15672 -p 5672:5672 rabbitmq:3-management
